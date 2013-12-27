a6b70313598cfd77f7a5abb5aa2c3a17e15d9596
showx <showshou@126.com>
2013-12-27 17:40:59 +0800

    cli

--- index.php
+++ index.php
@@ -3,10 +3,7 @@ define("PHPBODY",dirname(__FILE__));
 header("Content-type: text/html; charset=utf-8"); 
 include PHPBODY."/show/core/core.php";
 
-if(!defined('BODY'))
-{
-	exit();
-}
+if(!defined('BODY')){exit();}
 
 if(!file_exists(PHPBODY."/install.lok"))
 {
--- show/core/core.php
+++ show/core/core.php
@@ -4,53 +4,52 @@
  */
 error_reporting(E_ALL);
 define("BODY",1);
-
-//站群相关
-define("HOST",$_SERVER['HTTP_HOST']);
-
-//配置文件文件夹
-define("SHOWCONFIG",PHPBODY."/show/config");
-
-//模板文件目录
-define("SHOWTEMPLATE",PHPBODY."/show/display");
-
-//库文件
-define("SHOWLIBRARY",PHPBODY."/show/core/library");
-
-//函数文件 几个工具
-define("SHOWFUNCTION",PHPBODY."/show/core/function");
-
-//控制层
-define("SHOWCONTROL",PHPBODY."/show/control");
-
-//模型层
-define("SHOWMODEL",PHPBODY."/show/model");
-
-include SHOWCONFIG."/database.php";
-include SHOWCONFIG."/template.php";
-include SHOWLIBRARY."/autoload.lib.php";
-include SHOWCONTROL."/base.php";
-$GLOBALS['sdb'] = db::i();
-include SHOWLIBRARY."/session.lib.php";
-$GLOBALS['r'] = new r(); 
-global $r,$sdb;
-//route
-if(!isset($r->get->c))
+if(PHP_SAPI=='cli') //主要用于跑取cron 或 处理数据用
 {
-    $home = new home();
-    $home->index();
+    define("SHOWCONFIG",PHPBODY."/show/config");
+    define("SHOWLIBRARY",PHPBODY."/show/core/library");
+    define("SHOWFUNCTION",PHPBODY."/show/core/function");
 }else{
-    if(!isset($r->get->a))
+    //站群相关
+    define("HOST",$_SERVER['HTTP_HOST']);
+    //配置文件文件夹
+    define("SHOWCONFIG",PHPBODY."/show/config");
+    //模板文件目录
+    define("SHOWTEMPLATE",PHPBODY."/show/display");
+    //库文件
+    define("SHOWLIBRARY",PHPBODY."/show/core/library");
+    //函数文件 几个工具
+    define("SHOWFUNCTION",PHPBODY."/show/core/function");
+    //控制层
+    define("SHOWCONTROL",PHPBODY."/show/control");
+    //模型层
+    define("SHOWMODEL",PHPBODY."/show/model");
+
+    include SHOWCONFIG."/database.php";
+    include SHOWCONFIG."/template.php";
+    include SHOWLIBRARY."/autoload.lib.php";
+    $GLOBALS['sdb'] = db::i();
+    include SHOWLIBRARY."/session.lib.php";
+    $GLOBALS['r'] = new r(); 
+    global $r,$sdb;
+    //route
+    if(!isset($r->get->c))
     {
-        $r->get->a = 'index';
+        $home = new home();
+        $home->index();
+    }else{
+        if(!isset($r->get->a))
+        {
+            $r->get->a = 'index';
+        }
+        try{
+            @$tmp = new $r->get->c;
+        }catch(Exception $e)
+        {
+            echo 'no hack';exit();
+        }
+        $a = $r->get->a;
+        $tmp->$a();
     }
-    try{
-        @$tmp = new $r->get->c;
-    }catch(Exception $e)
-    {
-        echo 'no hack';exit();
-    }
-    $a = $r->get->a;
-    $tmp->$a();
 }
 ?>
