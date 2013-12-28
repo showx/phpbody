c5670c234b2feb03c9294d19df6007f7d2aeeeaa
show <showshou@126.com>
2013-12-28 17:43:10 +0800

    session

--- show/core/library/session.lib.php
+++ show/core/library/session.lib.php
@@ -4,7 +4,6 @@
  * Author show
  * copyright PHPBODY
  */
-global $sdb;
 function open_session() {
     return true;
 } 
@@ -12,7 +11,7 @@ function close_session() {
     return true;
 } 
 function read_session($sid) {
-    global $sdb;
+    $sdb =db::i();
     $result = $sdb->getone("SELECT data FROM show_session WHERE id='{$sid}'");
     if($result!=false)
     {
@@ -21,25 +20,25 @@ function read_session($sid) {
     return $result;
 } 
 function write_session($sid, $data) {
-    global $sdb;
+    $sdb =db::i();
     $time = time();
     $row = '';
     if($data != '')
     {
-        $result = $sdb->query("REPLACE INTO show_session (id, data,lastaccess) VALUES ('{$sid}', '{$data}','{$time}')");
+        $sdb->query("REPLACE INTO show_session (id, data,lastaccess) VALUES ('{$sid}', '{$data}','{$time}')");
         $row = $sdb->affected_rows();
     }
     return $row;	
 } 
 function destroy_session($sid) {
-	global $sdb;
-        $result = $sdb->query("DELETE FROM show_session WHERE id='{$sid}'");
+	$sdb =db::i();
+    $sdb->query("DELETE FROM show_session WHERE id='{$sid}'");
 	$_SESSION = array();
 	return $sdb->affected_rows();
 } 
 function clean_session($expire) {
-	global $sdb;
-        $sdb->query("DELETE FROM show_session WHERE DATE_ADD(lastaccess, INTERVAL %d SECOND) < NOW()");
+	$sdb =db::i();
+    $sdb->query("DELETE FROM show_session WHERE DATE_ADD(lastaccess, INTERVAL %d SECOND) < NOW()");
 	return $sdb->affected_rows();
 }
 session_set_save_handler('open_session', 'close_session', 'read_session', 'write_session', 'destroy_session', 'clean_session');
--- show/model/adminModel.d.php
+++ show/model/adminModel.d.php
@@ -6,14 +6,14 @@ class adminModel{
 	 * @param  [type] $pass [description]
 	 * @return [type]       [description]
 	 */
-	public function getAdmin($user,$pass)
+	public static function getAdmin($user,$pass)
 	{
 		global $sdb;
-		$sql = "select * from #pre#admin where username ={$user}";
+		$sql = "select * from #pre#admin where username ='{$user}'";
 		$tmp = $sdb->getone($sql);
 		if($tmp==false){return false;}
 		$password = md5($pass+$tmp['mcrypt']);
-		if($password == $pass)
+		if($password == $tmp['password'])
 		{
 			return true;
 		}else{
